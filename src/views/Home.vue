ad01b8c8fc536d0b18811118fd079a70083cdae2
Timeon1 <42144027+Timeon1@users.noreply.github.com>
2020-03-20 17:11:27 +0800

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-container">
+    首页
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style lang="less" scoped>
+  .home-container {
+    padding-top: 174px;
+    padding-bottom: 100px;
+  }
+</style>
